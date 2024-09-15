33204939623f199926d8efdd3f53e1b60216b012
VediSys <44647226+VediSys@users.noreply.github.com>
2024-09-15 17:36:04 -0600

    Update relative_strength.py

--- relative_strength.py
+++ relative_strength.py
@@ -30,39 +30,3 @@ def Relative(prix, n, DEC): # in-house function.
         rsi[q] = 100. -100. /(1. +r_s)
         #rsi.append(rsi)
     return rsi[:]
-
-"""
-def Parabolic(df_h, df_l,af, amax): #dec,
-    high, low = (df_h, df_l) #df['High'], df['Low']
-    sig0, xpt0, af0 = (True, high[0], af)
-    sar = [low[0] -(high -low).std()]
-
-    for i in range(1, len(df_h)):
-        sig1, xpt1, af01 = (sig0, xpt0, af0)
-        lmin = min( low[i -1], low[i])
-        lmax = max(high[i -1], high[i])
-        #
-        if sig1:
-            sig0 = low[i] >sar[-1]
-            xpt0 = max(lmax, xpt1)
-        else:
-            sig0 = high[i] >=sar[-1]
-            xpt0 = min(lmin, xpt1)
-        #
-        if sig0 == sig1:
-            sari = sar[-1] +(xpt1 -sar[-1]) *af01
-            af0 = min(amax, af01 +af)
-            if sig0:
-                af0 = af0 if xpt0 >xpt1 else af01
-                sari = min(sari, lmin)
-            else:
-                af0 = af0 if xpt0 <xpt1 else af01
-                sari = max(sari, lmax)
-        else:
-            af0 = af
-            sari = xpt0
-        #
-        sar.append(sari)
-        #sar = round(sar,dec)
-
-    return sar"""
